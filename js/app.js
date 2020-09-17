2e3e6337c73b7a9f66326c3f16d42a75026c2199
RACFENIX2020 <59393111+RACFENIX2020@users.noreply.github.com>
2020-09-17 17:41:06 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+//Inicializando Slider
+const slider =  document.querySelector('.slider');
+M.Slider.init(slider, {
+  indicator: false,
+  height: 520,
+  transition: 2000,
+  interval: 3500
+});
+
+
+      
